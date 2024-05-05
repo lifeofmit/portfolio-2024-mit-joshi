1397a4af1425912df263ebf41ee535f4130344c4
Mit Joshi <mitjoshi0207@gmail.com>
2024-05-06 01:41:24 +0530

    first commit

---
+++ js/cursor.js
@@ -0,0 +1,20 @@
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  // cursorOutline.style.left = `${posX}px`;
+  // cursorOutline.style.top = `${posY}px`;
+
+  cursorOutline.animate({
+    left: `${posX}px` ,
+    top: `${posY}px`
+  }, { duration: 500, fill: "forwards" });
+
+});
